0e4e2a452b2f0bd09c236227a26d02cbc16b85cd
Alessandro Camilli <a.camilli@yahoo.it>
2017-09-26 23:29:49 +0200

    fix search by type

--- l10n_it_fiscal_document_type/__init__.py
+++ l10n_it_fiscal_document_type/__init__.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-=======
-# Copyright 2017 Alessandro Camilli
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
->>>>>>> 8.0
 
 from . import model
